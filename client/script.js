fa5be7314e5907c88403ea2d3dc4bb017398441f
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2022-12-27 22:27:51 +0530

    updated script.js

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5001", {
+  const response = await fetch("https://aria-ta7z.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
